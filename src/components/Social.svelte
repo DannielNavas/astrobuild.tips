e9fbbeb10f88aaa9c767612fa1bf2451c89518b4
Danniel Navas <dannielnavas@gmail.com>
2023-04-19 08:56:03 -0500

    ✨ integracion svelte

---
+++ src/components/Social.svelte
@@ -0,0 +1,27 @@
+<ul
+  class="flex flex-wrap justify-center gap-6 md:gap-8 lg:justify-end lg:gap-12"
+>
+  <li>
+    <a class="text-gray-700 transition hover:text-gray-700/75" href="/">
+      About
+    </a>
+  </li>
+
+  <li>
+    <a class="text-gray-700 transition hover:text-gray-700/75" href="/">
+      Services
+    </a>
+  </li>
+
+  <li>
+    <a class="text-gray-700 transition hover:text-gray-700/75" href="/">
+      Projects
+    </a>
+  </li>
+
+  <li>
+    <a class="text-gray-700 transition hover:text-gray-700/75" href="/">
+      Blog
+    </a>
+  </li>
+</ul>
