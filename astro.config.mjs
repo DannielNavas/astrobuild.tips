9106deef5ef5c145c44cbf17dad83af74c5d80d6
Danniel Navas <dannielnavas@gmail.com>
2023-04-20 11:13:43 -0500

    ✨ seo, rss and sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 import mdx from "@astrojs/mdx";
-import tailwind from "@astrojs/tailwind";
-import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
 import vue from "@astrojs/vue";
-
+import { defineConfig } from "astro/config";
 import svelte from "@astrojs/svelte";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx(), react(), vue(), svelte()]
+  site: "http://localhost:3000",
+  integrations: [tailwind(), mdx(), react(), vue(), svelte(), sitemap()]
 });
